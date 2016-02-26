1c8080ed34693b4cdb85e6ac52e3ca084c214735
EK Thomson <ekthomson@gmail.com>
2016-02-25 19:31:44 -0700

    Change how boot is unpacked and add basic Debian handling

--- common/releasetools.py
+++ common/releasetools.py
@@ -3,12 +3,20 @@ import fnmatch
 
 def InstallBoot(info):
     # Install boot files
-    for filename in fnmatch.filter(info.input_zip.namelist(), "BOOT_EXTRA/*"):
+    for filename in fnmatch.filter(info.input_zip.namelist(), "BOOT_PARTITION/*"):
         fobj = info.input_zip.read(filename)
         common.ZipWriteStr(info.output_zip, filename, fobj)
     info.script.Mount("/boot")
-    info.script.UnpackPackageDir("BOOT_EXTRA", "/boot")
-    info.script.AppendExtra('package_extract_file("boot.img", "/boot/boot.img");')
+    info.script.UnpackPackageDir("BOOT_PARTITION", "/boot")
+
+def InstallDebian(info):
+    for filename in fnmatch.filter(info.input_zip.namelist(), "DEBIAN/*"):
+        fobj = info.input_zip.read(filename)
+        common.ZipWriteStr(info.output_zip, filename, fobj)
+    info.script.Mount("/debian")
+    info.script.UnpackPackageDir("DEBIAN", "/debian")
+
 
 def FullOTA_InstallEnd(info):
     InstallBoot(info)
+    InstallDebian(info)
